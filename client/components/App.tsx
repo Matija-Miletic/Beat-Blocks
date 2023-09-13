dfe76313d71f923f2807bb07528b583ddcc6e45c
Gabriel Rawcliffe <gabriel.rawcliffe@gmail.com>
2023-09-13 15:01:09 +1200

    chakra setup

--- client/components/App.tsx
+++ client/components/App.tsx
@@ -1,14 +1,25 @@
+import { Component } from 'react'
 import { useFruits } from '../hooks/useFruits.ts'
+import {
+  Box,
+  ChakraProvider,
+  Slider,
+  SliderFilledTrack,
+  SliderThumb,
+  SliderTrack,
+} from '@chakra-ui/react'
 
 function App() {
   const { data } = useFruits()
 
   return (
     <>
-      <div className="app">
-        <h1>Fullstack Boilerplate - with Fruits!</h1>
-        <ul>{data && data.map((fruit) => <li key={fruit}>{fruit}</li>)}</ul>
-      </div>
+      <ChakraProvider>
+        <div className="app">
+          <h1>Fullstack Boilerplate - with Fruits!</h1>
+          <ul>{data && data.map((fruit) => <li key={fruit}>{fruit}</li>)}</ul>
+        </div>
+      </ChakraProvider>
     </>
   )
 }
