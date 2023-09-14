0159e3b01d6daf94628c4ea0ac43d4d9062786b8
Gabriel Rawcliffe <gabriel.rawcliffe@gmail.com>
2023-09-14 20:06:46 +1200

    selectable synths

--- client/components/Freeform.tsx
+++ client/components/Freeform.tsx
@@ -2,39 +2,67 @@ import React, { useState, useEffect, useRef } from 'react'
 import * as Tone from 'tone'
 
 const SynthComponent = () => {
-  const [synth, setSynth] = useState(null)
+  const [synth, setSynth] = useState(new Tone.Synth().toDestination()) // Initialize with a default synth
   const [isPlaying, setIsPlaying] = useState(false)
+  const [selectedSynth, setSelectedSynth] = useState('Synth') // Default to 'Synth'
   const boxRef = useRef(null)
+  const isInsideBox = useRef(false)
+  let stopSynthTimeout
 
-  const startSynth = () => {
-    if (!isPlaying) {
-      const newSynth = new Tone.Synth().toDestination()
-      setSynth(newSynth)
-      setIsPlaying(true)
+  // Function to create and set the active synth based on the selected type
+  const createAndSetSynth = () => {
+    if (synth) {
+      synth.dispose() // Dispose of the previous synth
+    }
 
-      // Trigger the note immediately when starting the synth
-      if (boxRef.current) {
-        const box = boxRef.current.getBoundingClientRect()
-        const mouseY = box.height / 2 // You can set the initial Y position here
-        const boxHeight = box.height
+    let newSynth
 
-        playSynthNote(newSynth, mouseY, boxHeight)
-      }
+    // Create the selected synth type
+    newSynth = Tone[selectedSynth]
+      ? new Tone[selectedSynth]().toDestination()
+      : new Tone.Synth().toDestination()
+
+    setSynth(newSynth)
+  }
+
+  // Handle changes in the selected synth type
+  useEffect(() => {
+    createAndSetSynth()
+  }, [selectedSynth])
+
+  const startSynth = (initialMouseY) => {
+    setIsPlaying(true)
+
+    // Trigger the note immediately when starting the synth
+    if (boxRef.current) {
+      const box = boxRef.current.getBoundingClientRect()
+      const mouseY = initialMouseY - box.top // Get relative Y position in the box
+      const boxHeight = box.height
+
+      playSynthNote(synth, mouseY, boxHeight)
     }
   }
 
   const stopSynth = () => {
     if (synth) {
-      synth.triggerRelease()
-      setSynth(null)
-      setIsPlaying(false)
+      // Clear any existing stopSynthTimeout
+      if (stopSynthTimeout) {
+        clearTimeout(stopSynthTimeout)
+      }
+
+      // Delay the synth stop for a short period (e.g., 100 milliseconds)
+      stopSynthTimeout = setTimeout(() => {
+        synth.triggerRelease()
+        setIsPlaying(false)
+        isInsideBox.current = false
+      }, 1) // Adjust the delay duration as needed
     }
   }
 
   const playSynthNote = (synth, mouseY, boxHeight) => {
     // Calculate note and octave based on relative Y position (you can adjust the note range)
-    const minNote = 'C3'
-    const maxNote = 'C5'
+    const minNote = 'C1'
+    const maxNote = 'C8'
     const noteRange = [
       'C',
       'D',
@@ -58,15 +86,25 @@ const SynthComponent = () => {
 
   useEffect(() => {
     const handleMouseMove = (e) => {
-      if (synth && boxRef.current) {
+      if (boxRef.current) {
         const box = boxRef.current.getBoundingClientRect()
         const mouseY = e.clientY - box.top // Get relative Y position in the box
         const boxHeight = box.height
 
-        // Play the note continuously while the mouse is down
-        if (isPlaying) {
-          synth.triggerRelease()
-          playSynthNote(synth, mouseY, boxHeight)
+        if (mouseY >= 0 && mouseY <= boxHeight) {
+          // Mouse is inside the box
+          isInsideBox.current = true
+
+          // If the synth was not playing, start it
+          if (!isPlaying) {
+            startSynth(e.clientY)
+          } else {
+            // Synth is already playing, update the note
+            playSynthNote(synth, mouseY, boxHeight)
+          }
+        } else if (isInsideBox.current && isPlaying) {
+          // Mouse was inside but has moved outside, stop the synth
+          stopSynth()
         }
       }
     }
@@ -85,10 +123,26 @@ const SynthComponent = () => {
   return (
     <div>
       <h1>Interactive Synth</h1>
+      <div>
+        {/* Dropdown menu to select the synth type */}
+        <label htmlFor="synthSelect">Select Synth Type: </label>
+        <select
+          id="synthSelect"
+          value={selectedSynth}
+          onChange={(e) => setSelectedSynth(e.target.value)}
+        >
+          <option value="Synth">Synth</option>
+          <option value="AMSynth">AMSynth</option>
+          <option value="DuoSynth">DuoSynth</option>
+          <option value="FMSynth">FMSynth</option>
+          <option value="MembraneSynth">MembraneSynth</option>
+          <option value="PluckSynth">PluckSynth</option>
+        </select>
+      </div>
       <div
         ref={boxRef}
         className="synth-box"
-        onMouseDown={startSynth}
+        onMouseDown={(e) => startSynth(e.clientY)}
         onMouseUp={stopSynth}
         onMouseLeave={stopSynth}
       >
