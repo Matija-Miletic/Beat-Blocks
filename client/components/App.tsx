7dfa69747cff413a5d2eedd3721ee509afa38809
Matija Miletic <matijamiletic@protonmail.com>
2023-09-15 12:40:26 +1200

    Lasers and button working

--- client/components/App.tsx
+++ client/components/App.tsx
@@ -1,32 +1,48 @@
+// App.tsx
+import React, { useState } from 'react'
 import { ChakraProvider } from '@chakra-ui/react'
 import Sequencer from './Sequencer'
 import * as Buttons from './PlaybackButtons'
 import { ResetButton } from './ResetButton'
 import TempoSlider from './TempoSlider'
 import Oscillator from './Freeform'
+import { Lasers } from './Lasers'
+import { LaserButton } from './LaserButton'
 
 function App() {
+  const [isLaserActive, setIsLaserActive] = useState(false) // New state variable
   const handleTempoChange = (newTempo: number) => {
     console.log(`New Tempo: ${newTempo} BPM`)
+  }
+
+  // Function to toggle laser state
+  const toggleLaser = () => {
+    setIsLaserActive((prevState) => !prevState)
   }
 
   return (
     <ChakraProvider>
       <div className="app">
-        <h1>Beat Those Blocks!</h1>
-        <div className="button-container">
-          <Buttons.PlayButton />
-          <Buttons.PauseButton />
-          <Buttons.StopButton />
-          <Buttons.LoopButton />
-          <Buttons.RecordButton />
+        <div className="app-content">
+          <h1>Beat Those Blocks!</h1>
+          <div className="button-container">
+            <Buttons.PlayButton />
+            <Buttons.PauseButton />
+            <Buttons.StopButton />
+            <Buttons.LoopButton />
+            <Buttons.RecordButton />
+            <ResetButton />
+            <LaserButton toggleLaser={toggleLaser} />{' '}
+            {/* Passing the toggle function */}
+          </div>
+          <Sequencer />
+          <div className="slider-container">
+            <TempoSlider onChange={handleTempoChange} />
+          </div>
+          <Oscillator />
         </div>
-        <Sequencer />
-        <div className="slider-container">
-          <TempoSlider onChange={handleTempoChange} />
-        </div>
-        <ResetButton />
-        <Oscillator />
+        {isLaserActive && <Lasers />}{' '}
+        {/* Conditionally render based on state */}
       </div>
     </ChakraProvider>
   )
