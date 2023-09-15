1809451518cf0c6f2f33474914b02a3900b0ddd6
Kate Hooker <katehooker88@gmail.com>
2023-09-15 11:59:53 +0000

    onclick animation added to cells

--- client/components/Cell.tsx
+++ client/components/Cell.tsx
@@ -1,5 +1,5 @@
 import { useState } from 'react'
-import { RiCheckboxBlankFill, RiCheckboxBlankLine } from 'react-icons/ri' // Import icons
+import { RiCheckboxBlankFill, RiCheckboxBlankLine } from 'react-icons/ri'
 
 interface Props {
   trackNumber: number
@@ -7,18 +7,33 @@ interface Props {
 }
 
 export default function Cell({ trackNumber, cellNumber }: Props) {
-  const [isActive, setIsActive] = useState(false)
+  const [isActive, setIsActive] = useState<boolean>(false)
+  const [showImage, setShowImage] = useState<boolean>(false)
 
   function handleClick() {
     setIsActive(!isActive)
+
+    // Show the image immediately
+    setShowImage(true)
+
+    // Hide the image after 3 seconds, but start fading after 1 second
+    setTimeout(() => {
+      setShowImage(false)
+    }, 3000)
+
+    // Start the fade-out after 1 second
+    setTimeout(() => {
+      setShowImage(false)
+    }, 1000)
   }
 
-  const iconStyle = {
+  const iconStyle: React.CSSProperties = {
     width: '100%',
     height: '100%',
     display: 'flex',
     alignItems: 'center',
     justifyContent: 'center',
+    position: 'relative', // Add relative positioning to the container
   }
 
   return (
@@ -35,6 +50,21 @@ export default function Cell({ trackNumber, cellNumber }: Props) {
             <RiCheckboxBlankFill size="100%" />
           ) : (
             <RiCheckboxBlankLine size="100%" />
+          )}
+          {showImage && (
+            <img
+              src={`../../public/images/lego${cellNumber}.png`}
+              alt="Small"
+              className="animated-image"
+              style={{
+                position: 'absolute',
+                width: '80px', // Adjust the size as needed
+                height: 'auto', // Adjust the size as needed
+                animation: 'moveDown 2s ease-in-out, fadeOut 2s ease-in-out',
+                top: '0',
+                left: '0',
+              }}
+            />
           )}
         </div>
       </button>
