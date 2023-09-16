b9eac0f0daf066f14ec9ada72c380e5d099d53da
Cris Kiri <cris.kiri@gmail.com>
2023-09-17 00:15:40 +1200

    refactor: remove sound selector as tracks now have assigned drum part

--- client/components/Track.tsx
+++ client/components/Track.tsx
@@ -1,4 +1,3 @@
-import { useState } from 'react'
 import Cell from './Cell'
 
 interface Props {
@@ -6,30 +5,14 @@ interface Props {
   steps: number
 }
 
-// TODO: pass in available sounds as props for sound selection
-// TODO: populate sound selection with available sounds
-
 export default function Track({ trackNumber, steps }: Props) {
-  const [sound, setSound] = useState(1)
   const cells = [...Array(steps).keys()]
-
-  function onChange(e: React.ChangeEvent<HTMLSelectElement>) {
-    setSound(Number(e.target.value))
-  }
 
   return (
     <div id={`track-${trackNumber}`} className="track">
-      <select
-        name="soundselection"
-        id={`soundselection-${trackNumber}`}
-        onChange={onChange}
-        value={sound}
-      >
-        <option value="1">Clap</option>
-        <option value="2">Hihat</option>
-        <option value="3">Snare</option>
-        <option value="4">Kick</option>
-      </select>
+      <button style={{ minWidth: '60px', textAlign: 'left' }}>
+        {['Clap', 'Hihat', 'Snare', 'Kick'][trackNumber]}
+      </button>
       {cells.map((cell) => {
         return <Cell key={cell} trackNumber={trackNumber} cellNumber={cell} />
       })}
