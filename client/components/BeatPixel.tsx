cc51382c037d60c7febee3496b2b54952e831d1a
Cris Kiri <cris.kiri@gmail.com>
2023-09-19 10:59:18 +1200

    feat: cycle highlighting random pixel

--- client/components/BeatPixel.tsx
+++ client/components/BeatPixel.tsx
@@ -1,11 +1,54 @@
+import { useEffect, useState } from 'react'
 import * as Tone from 'tone'
+
 import Pixel from './Pixel'
-import { useState } from 'react'
+import * as lib from '../../lib/lib.ts'
+
+let prevPixelId = ''
 
 export default function BeatPixel() {
   const pixelCount = 64
   const pixelGrid = [...Array(pixelCount).keys()]
   const [isRunning, setIsRunning] = useState(false)
+
+  // TODO: Select a random pixel and highlight that pixel then play a sound
+  // TODO: Check all valid orthogonal pixels then move to a random one
+  // - Get all valid orthogonal pixels
+  // - Select a random one
+  // - Move to that pixel
+
+  // TODO: When a pixel is entered into, change to a random color then play a sound
+  // - Generate randomn color
+  // - Change pixel color
+  // - Generate random sound
+  // - Play sound
+
+  useEffect(() => {
+    setInterval(() => {
+      removePreviousPixelHighlight()
+      hightlightRandomPixel()
+    }, 1000)
+  })
+
+  function removePreviousPixelHighlight() {
+    console.log(`Prev Pixel ID: ${prevPixelId}`)
+    const prevPixel = document.getElementById(prevPixelId)
+    if (prevPixel) {
+      prevPixel.style.borderStyle = 'none'
+    }
+  }
+
+  function hightlightRandomPixel() {
+    const randomPixel = Math.floor(Math.random() * pixelCount)
+    const pixelId = `pixel-${randomPixel}`
+    console.log(`Prev Pixel ID: ${prevPixelId}`)
+    console.log(`Current Pixel ID: ${pixelId}`)
+    prevPixelId = pixelId
+    const pixel = document.getElementById(pixelId)
+    if (pixel) {
+      pixel.style.border = '2px solid black'
+    }
+  }
 
   function handleClick() {
     setIsRunning((prevValue) => !prevValue)
