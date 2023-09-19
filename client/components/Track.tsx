5f6421390274103a736b9a0e0f4b67502a21a1ea
Frances Krsinich <fkrsinich@yahoo.co.nz>
2023-09-19 14:02:38 +1200

    add working tracker lights

--- client/components/Track.tsx
+++ client/components/Track.tsx
@@ -12,11 +12,7 @@ export default function Track({ trackNumber, steps }: Props) {
   return (
     <div id={`track-${trackNumber}`} className="track">
       <button style={{ minWidth: '60px', textAlign: 'left' }}>
-        {
-          ['Progress', '808', 'Clap', 'Tap', 'Hihat', 'Snare', 'Kick'][
-            trackNumber
-          ]
-        }
+        {['', '808', 'Clap', 'Tap', 'Hihat', 'Snare', 'Kick'][trackNumber]}
       </button>
       {cells.map((cell) => {
         if (trackNumber === 0) {
